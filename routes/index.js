e27ca759f90784096a27d497067519ac6df1691d
GunnarSturla <gunnar@gunnarsturla.com>
2015-03-13 12:11:07 +0000

    Demo

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,31 @@
 var express = require('express');
 var router = express.Router();
-var cool = require('cool-ascii-faces');
+var pg = require('pg');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	console.log(cool());
+var dbresp;
+
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+      {
+       		console.error(err);
+       		dbresp = "Error " + err;
+       }
+      else
+       {
+       		dbresp = result.rows;
+       	}
+    });
+  });
+
+
   res.render('index', {
   	title: 'Concert Search',
-  	query: req.query.q
+  	query: req.query.q,
+  	db: dbresp
   });
   /*res.send({
   	users: ['Will', "Laura"]
