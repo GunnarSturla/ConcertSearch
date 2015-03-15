3a7535657f346c6d9d957b6c748807c23d8a6cdc
Thorleifur <tholli@heimsnet.is>
2015-03-15 20:00:41 +0000

    db

--- app.js
+++ app.js
@@ -56,6 +56,12 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+app.set('port', (process.env.PORT || 5000))
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+})
+var pg = require('pg');
 app.get('/db', function (request, response) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
         client.query('SELECT * FROM concert', function(err, result) {
@@ -66,7 +72,7 @@ app.get('/db', function (request, response) {
             { response.send(result.rows); }
         });
     });
-})
+});
 
 
 module.exports = app;
