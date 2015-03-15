84aa2f789428a91b04c680137fc5dc027bc19403
GunnarSturla <gunnar@gunnarsturla.com>
2015-03-15 20:51:44 +0000

    Revert faad21f..3a75356
    This rolls back to commit faad21f0aeaa39718b8b1ea03c733ddd19eb88d0.

--- app.js
+++ app.js
@@ -57,22 +57,5 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.set('port', (process.env.PORT || 5000))
-app.listen(app.get('port'), function() {
-    console.log("Node app is running at localhost:" + app.get('port'))
-})
-var pg = require('pg');
-app.get('/db', function (request, response) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query('SELECT * FROM concert', function(err, result) {
-            done();
-            if (err)
-            { console.error(err); response.send("Error " + err); }
-            else
-            { response.send(result.rows); }
-        });
-    });
-});
-
 
 module.exports = app;
