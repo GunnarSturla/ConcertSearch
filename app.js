dcb16fe6f467feee3305caeb0bc4b58c62d42611
Thorleifur <tholli@heimsnet.is>
2015-03-15 19:50:12 +0000

    /db

--- app.js
+++ app.js
@@ -56,6 +56,17 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+app.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM concert', function(err, result) {
+            done();
+            if (err)
+            { console.error(err); response.send("Error " + err); }
+            else
+            { response.send(result.rows); }
+        });
+    });
+})
 
 
 module.exports = app;
