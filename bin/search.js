7c0ce7f994b339d78128289274f0e295dd045dfc
fortena <arnigeir@gmail.com>
2015-04-13 19:42:23 +0000

    search js
    svona ætti þetta að virka

--- bin/search.js
+++ bin/search.js
@@ -7,20 +7,25 @@ var dbReady = false;
 
 exports.search = function(query, callback) {
 	var re = new RegExp("tónleikar","i");
-	var searchTerm = 'SELECT * FROM concerts WHERE';
+	var searchTerm = "SELECT * FROM concerts WHERE";
 	if(dbReady)
 	{
-		if (query.date)
-		{
-			searchDate = query.date;
-		}
+		query.term = "Björtuloft";
+		console.log(query.term);
 		if (query.term)
 		{
-			searchTerm = query.
+			searchTerm += " eventhallname LIKE %"+query.term+"% OR name LIKE %"+query.term+"% OR dateshow LIKE %";
+			searchTerm += ""+query.term+"% OR usergroupname LIKE %"+query.term+"% OR eventhallname LIKE %"+query.term;
+			searchTerm += "% OR price LIKE %"+query.term+"%";
+		}
+		if (query.date)
+		{
+			searchTerm += " AND dateshow LIKE %"+query.date+"%";
 		}
 	}
-		db.db.driver.execQuery("SELECT * FROM concerts WHERE  eventhallname LIKE '%Björtuloft%'",
-			['eventDateName'], callback);
+		db.db.driver.execQuery(searchTerm, callback);
+		/*db.db.driver.execQuery("SELECT * FROM concerts WHERE  eventhallname LIKE '%Björtuloft%'",
+			['eventDateName'], callback);*/
 		//concertsDB.find().where("id LIKE ??", [1]).run(callback);
 	//concertsDB.find({name: re},callback);
 
