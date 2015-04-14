249bb40c9c755a01676c92d4d475bbda014724b9
fortena <arnigeir@gmail.com>
2015-04-14 23:10:28 +0000

    Book Book
    + Fyrir og eftir

--- bin/book.js
+++ bin/book.js
@@ -5,15 +5,15 @@ var concertsDB = null;
 var seatsDB = null;
 var dbReady = false;
 
+//Notkun: book.getAvailableSeats(consertId, callback);
+//Fyrir: concertId er gilt Id fyrir ákveðna tónleika
+//Eftir: callback(err, resultArray) err eru villuskilaboð ef upp kemur villa, annars tómt;
+//			resultArray er boolean fylki með sætunum í salnum
+
 exports.getAvailableSeats = function(concertId, callback)
 {
 	if (dbReady)
 	{
-		//var searchTerm = "SELECT * FROM seats";// WHERE concertid = 2";//+concertId.id;
-		//console.log(searchTerm);
-		//db.db.driver.execQuery(searchTerm, callback);
-		//seatsDB.find({concertid: concertId}, callback);
-
 		var hallSize = 5;
 		var returnArray = [];
 		for (var i = 0; i < hallSize; i++)
@@ -28,26 +28,18 @@ exports.getAvailableSeats = function(concertId, callback)
 
 		function returnBuilder(callback)
 		{
-			//console.log('returnBuilder: '+ hallSize);
 
 			return function(err, result) {
 				if(err) callback(err, '');
-				//console.log('return function: '+result.length+ hallSize);
-				//console.log(result);
 
 				if(result) {
-					//console.log(result.length);
 					for (var i = 0; i < result.length; i++)
 					{
 						console.log("result[i] "+result[i].available);
-						//console.log('result.seat: '+result[i].seatno + ' row: '+result[i].rowno);
-						//console.log("seatno "+ result[i].seatno + " rowno "+ result[i].rowno);
 
 						if(result[i].available==='1') {
-							//console.log("i " + i);
 							returnArray[i%5][Math.floor(i/5)] = true;
 						}
-						//console.log(returnArray);
 						seatCount++;
 						if(seatCount === result.length) {
 							callback('', returnArray);
@@ -79,12 +71,18 @@ function generateBookingNumber()
 	return refNum;
 }
 
-exports.book = function(concertId, seatArr, callback)
+/*exports.book = function(concertId, seatArr, callback)
 {
 	callback(generateBookingNumber());
-}
+}*/
 
-exports.bookSeats = function(concertId, seatArr, callback) {
+
+// Notkun: book.book(concertId, seatArr, callback)
+// Fyrir: concertId er Id fyrir tónleika og seatArr er á forminu [[row,seat],[row,seat]...]
+//Eftir: callback(err, bookingNumber) err eru villuskilaboð ef upp kemur villa, annars tómt;
+//			bookingNumber er bókunarnúmerið
+
+exports.book = function(concertId, seatArr, callback) {
 
 	console.log('seatArr');
 	console.log(seatArr);
@@ -93,15 +91,9 @@ exports.bookSeats = function(concertId, seatArr, callback) {
 		var count = 0;
 		var totalBookings = seatArr.length;
 		function bookInDb(callback) {
-			//console.log('bookInDb');
-
 			return function(err, result) {
-				//console.log('return function');
-				//console.log(result);
-
 				result[0].save({ available:  bookingNumber}, function (err) {
 					if(err) callback(err,'');
-
 					count++;
 					if(count==totalBookings)
 						callback('', bookingNumber);
@@ -111,8 +103,6 @@ exports.bookSeats = function(concertId, seatArr, callback) {
 
 		for (var i = 0; i < seatArr.length; i++)
 		{
-			//console.log('seatArr:');
-			//console.log(seatArr[i]);
 			seatsDB.find({concertid: concertId, rowno: seatArr[i][0], seatno: seatArr[i][1]}, bookInDb(callback));
 		}
 
@@ -121,7 +111,6 @@ exports.bookSeats = function(concertId, seatArr, callback) {
 }
 
 db.onReady(function() {
-		console.log('calling back');
 		dbReady = true;
 
 		concertsDB = db.Concerts;
@@ -135,7 +124,7 @@ db.onReady(function() {
 			if(err) console.log(err);
 			console.log(results);
 		});*/
-		//exports.bookSeats(1, qArr, function(err, bookingNumber){if(!err) console.log('booking success! '+ bookingNumber);});
+		//exports.book(1, qArr, function(err, bookingNumber){if(!err) console.log('booking success! '+ bookingNumber);});
 
 
 	}
